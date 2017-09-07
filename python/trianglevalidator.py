b512fbdbbbdf362e18b95fc0ca655f8f5736f311
J. Max Mena <maxmylyn@gmail.com>
2017-09-07 15:11:47 -0700

    Updated comments

--- python/trianglevalidator.py
+++ python/trianglevalidator.py
@@ -6,9 +6,10 @@ def main():
         parser.add_argument('integers', metavar='N', type=int, nargs='+',
                             help='integers to form a triangle')
         arguments = parser.parse_args()
+        #  if x <= 0 || y <= 0 || z <=0 error out and say why
         if (arguments.integers[0] <= 0 or arguments.integers[1] <= 0 or arguments.integers[2] <=0):
             raise ValueError("ERROR: Arguments must be > 0")
-        # else if (x > (y + z) || y > (x + z) || z > (x + y))
+        #  else if (x > (y + z) || y > (x + z) || z > (x + y)) error out and say why
         elif (arguments.integers[0] > (arguments.integers[1] + arguments.integers[2]) or
             arguments.integers[1] > (arguments.integers[0] + arguments.integers[2]) or
             arguments.integers[2] > (arguments.integers[0] + arguments.integers[1])):
@@ -16,10 +17,11 @@ def main():
         #  if(x == y && y == z) - Equilateral
         if arguments.integers[0] == arguments.integers[1] and arguments.integers[1] == arguments.integers[2]:
             print "This is an Equilateral Triangle"
-        #   }else if(x == y || x == z || y == z) { - this is an Isosceles Triangle
+        #  else if(x == y || x == z || y == z) { - this is an Isosceles Triangle
         elif (arguments.integers[0] == arguments.integers[1] or arguments.integers[0] == arguments.integers[2] or
               arguments.integers[1] == arguments.integers[2]):
             print "This is an Isosceles Triangle"
+        #  else - must be a Scalene triangle
         else:
             print "This is a Scalene Triangle"
 
